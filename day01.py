48ec3b7d4a3a7b5f3665b41c484ddc0dee1a85f1
deplanty <yvon.deplante@gmail.com>
2025-12-01 18:05:34 +0100

    Day 1 Part 1 and 2 done

---
+++ day01.py
@@ -0,0 +1,80 @@
+from src import Debug, Reader
+
+
+class Cycle:
+    def __init__(self, start: int, limit: int):
+        self._value = start
+        self._limit = limit
+
+    def _cycle(self, value: int):
+        self._value += value
+        if self._value > self._limit:
+            self._value = self._value % self._limit
+
+    def cross_zero(self, delta: int) -> int:
+        return self._value + delta
+
+
+# Part 1
+
+with Reader(1, example=False) as reader:
+    total = 0
+    current = 50
+    for line in reader.iter_lines():
+        rotation = line[0]
+        delta = int(line[1:])
+        match rotation.lower():
+            case "r":
+                current += delta
+            case "l":
+                current -= delta
+            case _:
+                print(f"Unknown rotation {rotation}")
+
+        current %= 100
+
+        if current == 0:
+            total += 1
+
+print("Part 1:", total)
+
+
+# Part 2
+
+debug = Debug()
+debug.disable()
+
+debug("Cur", "Instr", "Next", "n", "total", sep="\t")
+with Reader(1, example=False) as reader:
+    total = 0
+    current = 50
+    for line in reader.iter_lines():
+        rotation = line[0]
+        delta = int(line[1:])
+        n, delta = divmod(delta, 100)
+
+        current_next = 0
+        match rotation.lower():
+            case "r":
+                current_next = current + delta
+            case "l":
+                current_next = current - delta
+            case _:
+                print(f"Unknown rotation {rotation}")
+
+        if current_next >= 100:
+            n += 1
+            current_next -= 100
+        elif current_next < 0:
+            n += 1
+            if current == 0:
+                n -= 1
+            current_next += 100
+        elif current_next == 0:
+            n += 1
+
+        total += n
+        debug(current, line, current_next, n, total, sep="\t")
+        current = current_next
+
+print("Part 2:", total)
