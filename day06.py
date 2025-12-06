b087985a876b9a26cdd69196373049177d7a89ea
deplanty <yvon.deplante@gmail.com>
2025-12-06 11:31:05 +0100

    Day 6: Part 1 and 2 done

---
+++ day06.py
@@ -0,0 +1,108 @@
+import re
+
+from src import Debug, Reader
+
+
+debug = Debug()
+
+
+# Part 1
+
+total = 0
+
+with Reader(6, example=False) as reader:
+    numbers: list[list[int]] = list() # type: ignore
+    operators: list[str] = list() # type: ignore
+    line: str
+    for line in reader.iter_lines():
+        line = line.lstrip(" ")
+        elements: list[str] = re.split(r"\s+", line)
+        if elements[0].isnumeric():
+            numbers.append([int(x) for x in elements]) # type: ignore
+        else:
+            operators = elements # type: ignore
+
+for index in range(len(operators)):
+    op = operators[index]
+    if op == "*":
+        column = 1
+    else:  # == "+""
+        column = 0
+
+    for i in range(len(numbers)):
+        column = eval(f"column {op} {numbers[i][index]}") # type: ignore
+    total += column
+
+print("Part 1:", total)
+
+
+# Part 2
+
+total = 0
+
+# The files have no trailing whitespaces
+with Reader(6, example=False) as reader:
+    # Get the numbers and the operators
+    lines = list()
+    for line in reader.fid.readlines():
+        line = line.rstrip("\n")
+        lines.append(line)
+
+# Add trailing spaces at the end of the lines if needed
+sizes = [len(l) for l in lines]
+size_max = max(sizes)
+for i in range(len(sizes)):
+    diff = size_max - len(lines[i])
+    if diff > 0:
+        lines[i] += " " * diff
+debug("Input")
+debug(*(repr(l) for l in lines), sep = "\n")
+
+debug("Extract operations")
+col: int = size_max - 1
+operations: list[list[str]] = list()
+operation: list[str] = list()
+find_operation: bool = False
+op: str = ""
+while col >= 0:
+    # Get the numbers
+    number: str = ""
+    for row in range(len(lines)):
+        char = lines[row][col]
+        # The operation is the last char to read
+        # Go to next column
+        if char == "*" or char == "+":
+            find_operation = True
+            op = char
+        elif char != " ":
+            number += char
+
+    if find_operation:
+        operation.append(number)
+        operation.insert(0, op)
+        operations.append(operation)
+        debug(operation)
+        operation = list()
+        col -= 1
+        find_operation = False
+    else:
+        operation.append(number)
+
+    col -= 1
+
+debug("Process operations")
+for op, *values in operations:
+    if op == "*":
+        column = 1
+    else:  # == "+""
+        column = 0
+
+    numbers: list[int] = [int(x) for x in values]
+
+    for x in numbers:
+        column = eval(f"column {op} {x}")
+    debug(op, numbers, column)
+    total += column
+
+
+print("Part 2:", total)
